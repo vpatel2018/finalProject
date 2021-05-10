8b6f18761817a6838a0e44f390e2961cc48b6dc1
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-10 12:55:18 -0400

    Update neuralNetwork.py

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -158,9 +158,8 @@ class NeuralNetwork:
         
         for x in range(0, len(self.outputUnits)):
             NeuralNetwork.updateOutputForOutputUnit(self, x)
+            #use sigmoid function for output of output units
         #
-        
-        #QUESTION: Do we use a sigmoid function for the output of output units?
         
     #
     
