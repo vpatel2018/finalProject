dcd4dd3112384dfc0cdd1026ff6ca0585fc3d7d8
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-11 18:21:54 -0400

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,1 +1,13 @@
+import neuralNetwork as nn
 
+if __name__ == "__main__":
+   
+   numInputUnits = 8
+   numHiddenUnits = 3
+   numOutputUnits = 8
+   lowerLimitForRandNums = -0.1
+   upperLimitForRandNums = 0.1
+   learningRate = 0.3
+   network = NeuralNetwork(numInputUnits, numHiddenUnits, numOutputUnits, lowerLimitForRandNums, upperLimitForRandNums, learningRate)
+    
+#
