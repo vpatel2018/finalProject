cd3de4f84e822ec61263640da30e6cca0b1b4730
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-11 14:28:31 -0400

    Update neuralNetwork.py

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -6,7 +6,7 @@ import math
 #used to create a node for a neural network
 class Neuron:
     def __init__(self, edgeWeights=[], inputs=[], error=0, output=0):
-        self.edgeWeights = edgeWeights #contains weights of edges that connect a unit to multiple other units
+        self.edgeWeights = edgeWeights #contains weights of edges that connect a unit to multiple other units (MAY NEED TO FIX)
         self.inputs = inputs #used to store input taken by a neural network
         self.error = error #represents error for a unit in a neural network
         self.output = output #represents output of a unit in a neural network
