2add0f39694babc2be9fde43c4e30fa2eb02580c
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-11 21:49:12 -0400

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,4 +1,5 @@
 import neuralNetwork as nn
+import math
 
 if __name__ == "__main__":
    
@@ -8,6 +9,14 @@ if __name__ == "__main__":
    lowerLimitForRandNums = -0.1
    upperLimitForRandNums = 0.1
    learningRate = 0.3
+   epochs = 5000
    network = nn.NeuralNetwork(numInputUnits, numHiddenUnits, numOutputUnits, lowerLimitForRandNums, upperLimitForRandNums, learningRate)
+   vectors = [[0] * 8 for x in range(8)]
+
+   for x in range(0, 8):
+       vectors[x][x] = 1
+   #
+   
+   trainingSet = [[vectors[x], vectors[x]] for x in range(8)]
    
 #
