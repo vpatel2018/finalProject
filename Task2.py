9182b764407d3d47fc879c416e8257e7a9d80e0a
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-13 21:19:45 -0400

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -2,12 +2,15 @@ import neuralNetwork as nn
 import math
 import os
 
-#TODO: check if requirements for task 2 are met, write comments
-
+#creates sum of squared errors file required for task 2
+#returns the name of the sum of squared errors file that is created
 def createSSEFile():
     
+   #create sum of squared errors file called SumOfSquaredErrors.csv
    sseFile = 'SumOfSquaredErrors.csv'
    file = open(sseFile, 'w')
+   
+   #write the columns SSE_output_unit1, ..., SSE_output_unit8 in SumOfSquaredErrors.csv
    string = ""
    for x in range(0, 8):
        string += ('SSE_output_unit' + str(x + 1) + ',')   
@@ -20,37 +23,54 @@ def createSSEFile():
     
 #
 
+#creates eight hidden unit encoding files required for task 2
+#returns a list containing names of the hidden unit encoding files created
 def createHiddenUnitEncodingFiles():
-    
+   
+   #represents list of all input vectors for task 2
    vectors = [[0] * 8 for x in range(8)]
 
    for x in range(0, 8):
        vectors[x][x] = 1
    #
     
+   #list containing names of the hidden unit encoding files created
    hiddenUnitEncodingFiles = []
    
    for x in range(0, 8):
+       
        word = ''
        for y in range(0, len(vectors[x])):
            vectors[x][y] = str(vectors[x][y])
        #
        word = word.join(vectors[x])
+       
+       #create hidden unit encoding file
        title = 'HiddenUnitEncoding_' + word + '.csv'
        hiddenUnitEncodingFiles.append(title)
        file = open(title, 'w')
+       
+       #write the columns HiddenUnit1Encoding, ..., HiddenUnit3Encoding in hidden unit encoding file
        file.write('HiddenUnit1Encoding,HiddenUnit2Encoding,HiddenUnit3Encoding\n')
        file.close()
+       
    #
    
    return hiddenUnitEncodingFiles
 
 #
 
+#used to compute sum of square error
 def computeSumOfSquareError(predictedValues, expectedValues):
+    
+    '''
+    predictedValues -> list of predicted values
+    expectedValues -> list of expected values
+    '''
     
     sumOfSquareErrors = 0
     
+    #take summation of differences between predicted and expected values
     for x in range(0, len(predictedValues)):
         sumOfSquareErrors += math.pow(expectedValues[x] - predictedValues[x], 2)
     #
@@ -59,9 +79,20 @@ def computeSumOfSquareError(predictedValues, expectedValues):
        
 #
 
+#writes a new line to a csv file
+#takes into account the number of digits that should come after a decimal place for a value that is written to a csv file
 def appendListContentsToFile(fileName, array, numDigitsAfterDecimalPlace):
- 
+    
+    '''
+    fileName -> represents name of csv file we are going to write to
+    array -> represents list of values we have to include when writing a new line in a csv file
+    numDigitsAfterDecimalPlace -> represents number of digits that should come after a decimal place for a value
+    '''
+    
+    #open a csv file
     file = open(fileName, 'a')
+    
+    #write a new line in a csv file
     string = ""
     for z in array:
         if(numDigitsAfterDecimalPlace == 0):
@@ -76,7 +107,14 @@ def appendListContentsToFile(fileName, array, numDigitsAfterDecimalPlace):
     
 #
 
+#writes a new line to a csv file
+#does not take into account # of digits that should come after a decimal place for a value that is written to a csv file
 def appendListContentsToFile2(fileName, array):
+    
+    '''
+    fileName -> represents name of csv file we are going to write to
+    array -> represents list of values we have to include when writing a new line in a csv file
+    '''
     
     file = open(fileName, 'a')
     string = ""
